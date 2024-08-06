2981a590e95ed39eaa85a08a6186f30db6eb767a
Laimonas <ldirgelas530@gmail.com>
2024-08-06 19:49:11 +0300

    up

---
+++ Function_Declaration.js
@@ -0,0 +1,14 @@
+// FUnction Declaration
+console.log(addDollarSign(100));
+
+function addDollarSign(value) {
+    return '$' + value;
+}
+
+console.log(addPLusSign(200));
+
+// Function Expresion
+
+const addPLusSign = function (value) {
+    return '+' + value;
+}
---
+++ Invoked_Expressions.js
@@ -0,0 +1,21 @@
+
+
+(function () {
+    
+    const user = 'Jhn';
+    console.log(user);
+    const hello = () => console.log('Hello from the Ifre');
+    hello();
+})();
+
+
+(function(name){
+    console.log('Hello ' + name);
+
+})('Shawn');
+
+(function hello(){
+    console.log('Hello');
+    
+})();
+
